d7422fa974db667c3376c2166db0390ebb7f6f3d
Melvin NAMONT <melvin.namont@gmail.com>
2020-11-13 11:54:40 +0100

    init

---
+++ app/src/main/java/es/upm/etsiinf/news_manager/MainActivity.java
@@ -0,0 +1,16 @@
+package es.upm.etsiinf.news_manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
