943e0836eeef410f8bea427d31e96cd897135662
Melvin NAMONT <melvin.namont@gmail.com>
2020-11-25 19:44:53 +0100

    added the remember me feature

--- app/src/main/java/es/upm/etsiinf/news_manager/LoginActivity.java
+++ app/src/main/java/es/upm/etsiinf/news_manager/LoginActivity.java
@@ -1,9 +1,12 @@
 package es.upm.etsiinf.news_manager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
@@ -13,6 +16,8 @@ import es.upm.etsiinf.news_manager.utils.network.ModelManager;
 import es.upm.etsiinf.news_manager.utils.network.exceptions.AuthenticationError;
 
 public class LoginActivity extends AppCompatActivity{
+
+    private static final String REMEMBER_PREF = "rememberPreferences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -38,7 +43,18 @@ public class LoginActivity extends AppCompatActivity{
     private void login(String username, String password) {
         new Thread ( () ->{
             try {
+                CheckBox rememberMeCheckbox = findViewById(R.id.checkbox_remember_me);
                 ModelManager.login(username, password);
+                if( rememberMeCheckbox.isChecked()){
+                    //TODO : save username and password in a file
+                    SharedPreferences rememberPreferences = getApplicationContext().getSharedPreferences(REMEMBER_PREF, MODE_PRIVATE);
+                    SharedPreferences.Editor rememberPreferencesEditor = rememberPreferences.edit();
+                    rememberPreferencesEditor.putString("username", ModelManager.getIdUser());
+                    rememberPreferencesEditor.putString("apiKey", ModelManager.getLoggedApiKey());
+                    Log.e("APIKEY", ModelManager.getLoggedApiKey());
+                    rememberPreferencesEditor.putString("authType", ModelManager.getLoggedAuthType());
+                    rememberPreferencesEditor.commit();
+                }
                 Intent i_nextActivity = new Intent(this, MainActivity.class);
                 this.startActivity(i_nextActivity);
             }
--- app/src/main/java/es/upm/etsiinf/news_manager/MainActivity.java
+++ app/src/main/java/es/upm/etsiinf/news_manager/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +30,7 @@ import es.upm.etsiinf.news_manager.utils.network.exceptions.ServerCommunicationE
 public class MainActivity extends AppCompatActivity
 {
     private List<Article> articleList = null;
+    private static final String REMEMBER_PREF = "rememberPreferences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -38,6 +42,15 @@ public class MainActivity extends AppCompatActivity
         if(!ModelManager.isConfigured()){
             initProperties();
         }
+
+        SharedPreferences rememberPreferences = getApplicationContext().getSharedPreferences(REMEMBER_PREF, MODE_PRIVATE);
+        if(rememberPreferences.getString("apiKey", null) != null){
+            String username = rememberPreferences.getString("username", null);
+            String apiKey = rememberPreferences.getString("apiKey", null);
+            String authType = rememberPreferences.getString("authType", null);
+            ModelManager.stayloggedin(username, apiKey, authType);
+        }
+
 
         /* Button to go to the login activity */
         Button goToLoginButton = findViewById(R.id.btn_goToLogin);
@@ -53,10 +66,15 @@ public class MainActivity extends AppCompatActivity
         }
         signOutButton.setOnClickListener( v -> {
             ModelManager.logout();
+            SharedPreferences.Editor rememberPreferencesEditor = rememberPreferences.edit();
+            rememberPreferencesEditor.clear();
+            rememberPreferencesEditor.commit();
             signOutButton.setVisibility(Button.INVISIBLE);
             goToLoginButton.setVisibility(Button.VISIBLE);
             downloadArticles();
         });
+
+
     }
 
     @Override
